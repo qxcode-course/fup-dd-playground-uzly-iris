5a4851fcff63744f79b795f632978516ba2549b4
uzly-iris <uzlyiris@alu.ufc.br>
2026-03-17 18:07:41 +0000

    Drink me up, there's wine in every word

--- fup/bala/go/draft.go
+++ fup/bala/go/draft.go
@@ -6,13 +6,11 @@ import (
 )
 
 func main() {
-	var x1, y1, x2, y2, d, x, y, qx, qy float64
+	var x1, y1, x2, y2, d, qx, qy float64
 	fmt.Scan(&x1, &y1)
 	fmt.Scan(&x2, &y2)
-	x = x2 - x1
-	y = y2 - y1
-	qx = math.Pow(x, 2)
-	qy = math.Pow(y, 2)
+	qx = math.Pow((x2 - x1), 2)
+	qy = math.Pow((y2 - y1), 2)
 	d = math.Sqrt(qx + qy)
 	fmt.Printf("%.2f\n", d)
 }
