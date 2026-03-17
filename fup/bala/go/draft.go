92d09aabd5b44f49f456d80aa50dfac9ae3c3fd3
uzly-iris <uzlyiris@alu.ufc.br>
2026-03-17 17:33:11 +0000

    Dei uma otimizada no último codigo

--- fup/bala/go/draft.go
+++ fup/bala/go/draft.go
@@ -7,10 +7,8 @@ import (
 
 func main() {
 	var x1, y1, x2, y2, d, x, y, qx, qy float64
-	fmt.Scan(&x1)
-	fmt.Scan(&y1)
-	fmt.Scan(&x2)
-	fmt.Scan(&y2)
+	fmt.Scan(&x1, &y1)
+	fmt.Scan(&x2, &y2)
 	x = x2 - x1
 	y = y2 - y1
 	qx = math.Pow(x, 2)
